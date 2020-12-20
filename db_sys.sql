8297e698b82c2a1e0070f40bcd655a47de5bd507
HHHYYN <15539018727@163.com>
2020-12-20 20:30:59 +0800

    Update db_sys.sql

--- db_sys.sql
+++ db_sys.sql
@@ -1,17 +1,4 @@
-/*
-Navicat MySQL Data Transfer
 
-Source Server         : mysql
-Source Server Version : 50621
-Source Host           : localhost:3306
-Source Database       : db_sys
-
-Target Server Type    : MYSQL
-Target Server Version : 50621
-File Encoding         : 65001
-
-Date: 2020-05-10 10:21:57
-*/
 
 SET FOREIGN_KEY_CHECKS=0;
 
